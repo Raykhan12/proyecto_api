9e6f04cef421e41de060f98e4bb5e3560ac7e071
Raykhan12 <45717532+Raykhan12@users.noreply.github.com>
2022-01-19 00:11:31 -0300

    modified  StrockView and add user/superuser

--- README.md
+++ README.md
@@ -3,7 +3,13 @@
 # Python/Django Challenge
 
 ## Description
+super user:
+User: admin
+Pass: 123
 
+regular user
+User: odin
+pass:123
 
 ## How to run the project
 
--- stock_service/stocks/views.py
+++ stock_service/stocks/views.py
@@ -1,14 +1,38 @@
 # encoding: utf-8
 
+from os import sep
+from telnetlib import STATUS
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import pandas as pd
+import json
+
 
 
 class StockView(APIView):
     """
     Receives stock requests from the API service.
     """
+    
     def get(self, request, *args, **kwargs):
+         
         stock_code = request.query_params.get('stock_code')
-        # TODO: Make request to the stooq.com API, parse the response and send it to the API service.
-        return Response()
+        
+        try:
+            #with request.Session() as s:
+                stock_info=f'https://stooq.com/q/l/?s={stock_code}&f=sd2t2ohlcvn&h&e=csv'
+                stock_df=pd.read_csv(stock_info)
+                
+                stock_json=stock_df.to_json(orient='records',lines=True)
+                status=200
+
+                if str(stock_df["Date"][0])=="N/D":
+                    stock_json='{"Error": "Error invalid sotck name"}'     
+                    status=404
+                    
+        except Exception as e:
+            stock_json='{"Error": "Error to Fech stock information"}'     
+            status=500
+
+        finally:
+            return Response(json.loads(stock_json),status=status)

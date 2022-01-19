73c5ee67d6a45c643818aec5e3fa82a5b48b704b
Raykhan12 <45717532+Raykhan12@users.noreply.github.com>
2022-01-19 00:44:24 -0300

    Modified stockView from api

--- api_service/api/views.py
+++ api_service/api/views.py
@@ -3,19 +3,46 @@
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
 from api.models import UserRequestHistory
 from api.serializers import UserRequestHistorySerializer
 
+import requests
+
+API_STOCK_URL="http://127.0.0.1:7000/"
 
 class StockView(APIView):
     """
     Endpoint to allow users to query stocks
     """
+    
     def get(self, request, *args, **kwargs):
-        stock_code = request.query_params.get('q')
-        # TODO: Call the stock service, save the response, and return the response to the user
-        return Response()
+        
+        stock_code = request.query_params.get('stock_code')
+       
+        try:   
+            response = requests.request(
+            "GET", API_STOCK_URL+"stock", params ={'stock_code': f'{stock_code}'})
+
+            if response.status_code==200:
+                self.save_db(response.json(),request.user)
+                
+            return Response(response.json(),response.status_code)
+
+        except Exception as e:
+            return Response({"Error": "Error to Fech stock information"}, status=500)
+
+    def save_db(self,data,user):
+
+        userRequestHistory = UserRequestHistory()
+        userRequestHistory.user = user
+        userRequestHistory.date = data["Date"] + " " + data["Time"]
+        userRequestHistory.name = data["Name"]
+        userRequestHistory.symbol = data["Symbol"]
+        userRequestHistory.open = data["Open"]
+        userRequestHistory.high = data["High"]
+        userRequestHistory.low = data["Low"]
+        userRequestHistory.close = data["Close"]
+        userRequestHistory.save()
 
 
 class HistoryView(generics.ListAPIView):
